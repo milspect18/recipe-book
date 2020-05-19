887197290470fe42257140311efcb0db9ce59fa7
Kyle Price <kyleprice@yahoo.com>
2020-05-18 20:30:53 -0400

    Add a rating display component

---
+++ src/components/StarRating.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const StarRating = ({ className, count, minimize }) => {
+    const star = "❤️";
+    const numStars = Math.min(Math.max(0, count), 5);
+
+    return (
+        <div className={className} >
+            {
+                minimize ? (
+                    <p style={{ whiteSpace: "nowrap", overflow: "hidden" }} >{`${numStars}${star}`}</p>
+    ) : (
+        [...Array(numStars).keys()].map((_, idx) => <p key={idx}>{star}</p>)
+    )
+}
+        </div >
+    );
+};
+
+export default StarRating;
