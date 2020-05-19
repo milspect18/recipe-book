5ef83965576418536f867ab30728c5cf978b9184
Kyle Price <kyleprice@yahoo.com>
2020-05-19 16:54:39 -0400

    add content and earmark style class names

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,8 +7,15 @@ const LoginPage = () => {
     };
 
     return (
-        <div>
-            <button onClick={handleLogin} >Login</button>
+        <div className="login-box">
+            <h1 className="login-box__title">RecipeBook</h1>
+            <p className="login-box__phrase">A path to happy tastebuds!</p>
+            <button
+                className="login-box__button"
+                onClick={handleLogin}
+            >
+                Login
+            </button>
         </div>
     );
 };
