27480b7383dc55a551d79bd1427dd70d64b4962f
Kyle Price <kyleprice@yahoo.com>
2020-05-19 15:29:19 -0400

    Correct history prop vs object prop typo

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,7 +6,7 @@ import RecipeApp from "../components/RecipesApp";
 const history = createBrowserHistory();
 
 const AppRouter = () => (
-    <Router history={{ history }}>
+    <Router history={history} >
         <Switch>
             <Route exact path="/" component={RecipeApp} />
             <Route component={<p>Unknown Path</p>} />
