f1b45cac74c2d2159287dbe1fd1bd645bed9413d
Kyle Price <kyleprice@yahoo.com>
2020-05-19 15:22:54 -0400

    Add placeholder 404

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,6 +9,7 @@ const AppRouter = () => (
     <Router history={{ history }}>
         <Switch>
             <Route exact path="/" component={RecipeApp} />
+            <Route component={<p>Unknown Path</p>} />
         </Switch>
     </Router>
 );
