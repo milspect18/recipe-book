2e4beadddaf5564cadaa0c8fd5ba0f1c41b2e48a
Kyle Price <kyleprice@yahoo.com>
2020-05-18 20:31:28 -0400

    Add recipe item content and responsive design

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import StarRating from "./StarRating";
+import breakpoints from "../styles/base/_settings.scss";
 
-const RecipeItem = ({ recipe }) => {
+
+const RecipeItem = ({ recipe, style }) => {
+    const smallScreen = parseInt(breakpoints.s_screen);
+    const xSmallScreen = parseInt(breakpoints.xs_screen);
+    const xxSmallScreen = parseInt(breakpoints.xxs_screen);
+
+    const [isSmall, setIsSmall] = useState(window.innerWidth < smallScreen);
+    const [isXSmall, setIsXSmall] = useState(window.innerWidth < xSmallScreen);
+    const [isXxSmall, setIsXxSmall] = useState(window.innerWidth < xxSmallScreen);
+
+    const handleWinResize = () => {
+        const width = window.innerWidth;
+
+        if (width < smallScreen) {
+            setIsSmall(true);
+        } else {
+            setIsSmall(false);
+        }
+
+        if (width < xSmallScreen) {
+            setIsXSmall(true);
+        } else {
+            setIsXSmall(false);
+        }
+
+        if (width < xxSmallScreen) {
+            setIsXxSmall(true);
+        } else {
+            setIsXxSmall(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleWinResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWinResize);
+        }
+    }, []);
 
     return (
-        <div className="content-container" >
-            <p>{`${recipe.name} is rated ${recipe.rating} stars.  It take ${recipe.prepTime + recipe.cookTime} minutes to make.`}</p>
+        <div style={style} className="content-container" onClick={() => { console.log("Clicked") }} >
+            <div className="recipe-list-item">
+                <div className="recipe-list-item__img-crop" >
+                    <img alt={recipe.name} src={recipe.img} className="recipe-list-item__img" />
+                </div>
+
+                <div className="recipe-list-item__details" >
+                    <p className="recipe-list-item__title" >{recipe.name}</p>
+
+                    {isSmall ? (
+                        <p className="recipe-list-item__times" >
+                            {recipe.prepTime + recipe.cookTime} min
+                        </p>
+                    ) : (
+                            <p className="recipe-list-item__times" >
+                                Prep: {recipe.prepTime} min | Cook: {recipe.cookTime} min
+                            </p>
+                        )}
+                </div>
+
+                {
+                    !isXxSmall &&
+                    <StarRating
+                        className="recipe-list-item__rating"
+                        count={recipe.rating}
+                        minimize={isXSmall}
+                    />
+                }
+            </div>
         </div>
     );
 };
--- src/components/RecipesApp.js
+++ src/components/RecipesApp.js
@@ -1,22 +1,40 @@
 import React, { useReducer } from "react";
 import recipesReducer from "../reducers/recipes";
 import RecipesContext from "../context/recipes-context";
-import AddIngredientsForm from "./AddIngredientForm";
+// import AddIngredientsForm from "./AddIngredientForm";
 import RecipeItem from "./RecipeItem";
 
+const tempRecipe = {
+    name: "Double Baked Spaghetti",
+    rating: 10,
+    prepTime: 20,
+    cookTime: 30,
+    img: "https://hips.hearstapps.com/delish/assets/17/37/1505340657-baked-spaghetti-delish-1.jpg"
+}
+
+const tempRecipeTwo = {
+    name: "Triple Chocolate Fountain of Death",
+    rating: 5,
+    prepTime: 20,
+    cookTime: 30,
+    img: "https://hips.hearstapps.com/delish/assets/17/37/1505340657-baked-spaghetti-delish-1.jpg"
+}
+
 const RecipesApp = () => {
-    const [recipes, recipesDispatch] = useReducer(recipesReducer, []);
+    const [recipes] = useReducer(recipesReducer, []);
 
     return (
         <RecipesContext.Provider value={{ recipes }}>
-            <AddIngredientsForm
-                recipe={{
-                    name: "Spaghetti",
-                    rating: 4,
-                    prepTime: 20,
-                    cookTime: 30
-                }}
-            />
+            <RecipeItem recipe={tempRecipe} />
+            <RecipeItem style={{ background: "#F0F0F0" }} recipe={tempRecipeTwo} />
+            <RecipeItem recipe={tempRecipe} />
+            <RecipeItem style={{ background: "#F0F0F0" }} recipe={tempRecipeTwo} />
+            <RecipeItem recipe={tempRecipe} />
+            <RecipeItem style={{ background: "#F0F0F0" }} recipe={tempRecipeTwo} />
+            <RecipeItem recipe={tempRecipe} />
+            <RecipeItem style={{ background: "#F0F0F0" }} recipe={tempRecipeTwo} />
+            <RecipeItem recipe={tempRecipe} />
+            <RecipeItem style={{ background: "#F0F0F0" }} recipe={tempRecipeTwo} />
         </RecipesContext.Provider>
     );
 };
