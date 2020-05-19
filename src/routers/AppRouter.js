d7b6880c2e214b31cf8e07cdd7ae64785da0ff35
Kyle Price <kyleprice@yahoo.com>
2020-05-19 16:20:43 -0400

    Render LoginPage as base route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import RecipeList from "../components/RecipeList";
+import LoginPage from "../components/LoginPage";
 
 const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history} >
         <Switch>
-            <Route exact path="/" component={RecipeApp} />
+            <Route exact path="/" component={LoginPage} />
             <Route component={<p>Unknown Path</p>} />
         </Switch>
     </Router>
