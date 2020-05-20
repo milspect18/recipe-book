9d299db47cf103772296d7ca6f55f12e7c55c1b2
Kyle Price <kyleprice@yahoo.com>
2020-05-20 17:08:39 -0400

    Create and export a component named Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <div>
+            This will be the header content
+        </div>
+    );
+};
+
+export default Header;
