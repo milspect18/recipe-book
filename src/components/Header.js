29ae9016536b8da5fd5d6402e924362feb04ad52
Kyle Price <kyleprice@yahoo.com>
2020-05-20 19:19:49 -0400

    Add placeholder content and initial styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,15 @@ import React from "react";
 const Header = () => {
 
     return (
-        <div>
-            This will be the header content
+        <div className="content-container" >
+            <div className="header-box" >
+                <img src="/images/logo.png" className="header-box__logo" />
+
+                <div className="header-box__info-container" >
+                    <p className="header-box__info-text">User Name</p>
+                    <p className="header-box__info-text">Favorite Recipes</p>
+                </div>
+            </div>
         </div>
     );
 };
